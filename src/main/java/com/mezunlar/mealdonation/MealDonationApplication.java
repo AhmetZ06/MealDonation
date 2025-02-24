25f173a3ab837f80b1550921974117eaefd533f9
Ahmet Dolma <aahmet.dolma@gmail.com>
2025-02-25 00:50:07 +0300

    Initialization

--- src/main/java/com/mezunlar/mealdonation/MealDonationApplication.java
+++ src/main/java/com/mezunlar/mealdonation/MealDonationApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MealDonationApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MealDonationApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MealDonationApplication.class, args);
+    }
 
 }
--- src/test/java/com/mezunlar/mealdonation/MealDonationApplicationTests.java
+++ src/test/java/com/mezunlar/mealdonation/MealDonationApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class MealDonationApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
