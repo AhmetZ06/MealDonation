2e5c9e1e7332b5254826e6b66ae6780b9283b4dd
Ahmet Dolma <aahmet.dolma@gmail.com>
2025-02-25 00:46:36 +0300

    My MealDonation Project first step with intellij

---
+++ src/main/java/com/mezunlar/mealdonation/MealDonationApplication.java
@@ -0,0 +1,13 @@
+package com.mezunlar.mealdonation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MealDonationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MealDonationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mezunlar/mealdonation/MealDonationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mezunlar.mealdonation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MealDonationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
